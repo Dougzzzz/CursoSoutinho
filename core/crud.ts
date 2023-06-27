3dcfa7f7f562b95c9f88043c272f8bdce5a41375
dougzzzz <douglasbarcelos02@gmail.com>
2023-06-26 21:27:07 -0300

    módulo update

--- core/crud.ts
+++ core/crud.ts
@@ -1,4 +1,5 @@
 import fs, { readFileSync } from "fs" 
+import { v4 as uuid } from 'uuid';
 //const fs = require("fs") CommonJS;
 const DB_FILE_PATH = "./core/db"
 
@@ -6,13 +7,15 @@ console.log("[CRUD]");
 
 
 interface ToDo {
+  id: string;
   date: string;
   content:string;
   done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): ToDo {
   const toDo: ToDo = {
+    id: uuid(),
     date: new Date().toISOString(), //transformar a data em string, o Date retorna um objeto tipo data
     content: content,
     done: false,
@@ -29,7 +32,7 @@ function create(content: string) {
     dogs: [],
     }, null,
      2));// converter o toDo em string
-  return content;
+  return toDo;
 }
 
 function read(): Array<ToDo> {
@@ -44,11 +47,46 @@ function read(): Array<ToDo> {
   return db.toDos;
 
 }
+
+function update(id: string, partialToDo: Partial<ToDo>){
+  let updatedToDo;
+  const toDos = read();
+  toDos.forEach((currentToDo)=> {
+    const isToUpdate = currentToDo.id === id;
+      if(isToUpdate) {
+        updatedToDo = Object.assign(currentToDo, partialToDo);
+      }
+  });
+
+  fs.writeFileSync(DB_FILE_PATH,JSON.stringify({
+    toDos,
+  }, null, 2))
+
+  if(!updatedToDo){
+    throw new Error("Please, provide another ID!")
+  }
+  
+}
+
+function updateContentById(id: string, content: string) {
+  return update(id, {
+    content,
+  });
+}
+
+
+
 function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH,"");
 }
+
 // [SIMULATION]
 CLEAR_DB()
 create("primeira TO DO");
-create("segunda TO DO");
+create("primeira TO DO");
+const terceiraToDo = create("segunda TO DO");
+// update (terceiraToDo.id, {
+//   content:"Atualizada"
+// })
+updateContentById(terceiraToDo.id, "Atualizada!")
 console.log(read());
